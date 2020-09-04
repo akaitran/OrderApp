5d20be1c667e1895fe70fefb21da63589f2f764f
akaitran <tqa.khuong@gmail.com>
2020-09-04 22:33:55 +1000

    Change files structure

--- api/printAPIs/print-kitchen-usb.php
+++ api/printAPIs/print-kitchen-usb.php
@@ -222,7 +222,7 @@ try {
     $printer -> setTextSize(3,3);
     $printer -> setJustification(Printer::JUSTIFY_CENTER);
 
-    $tux = EscposImage::load("../../images/codologo.png", false);
+    $tux = EscposImage::load("../../assets/images/codologo.png", false);
     $printer->bitImage($tux);
 
     if ($order->type !== "dine in") {
--- index.php
+++ index.php
@@ -12,48 +12,48 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-		<link rel="shortcut icon" type="image/png" href="images/logo.png"/>
+		<link rel="shortcut icon" type="image/png" href="assets/images/logo.png"/>
 
 		<!-- Animation library for notifications   -->
-		<link href="css/animate.min.css" rel="stylesheet"/>
-		<link href="css/jquery-ui.css" rel="stylesheet"/>
-        <link href="css/bootstrap.min.css"rel="stylesheet"/>
-        <link href="css/material.min.css" rel="stylesheet"/>
-        <link href="css/angular-material.min.css" rel="stylesheet"/>
-        <link href="css/light-bootstrap-dashboard.css" rel="stylesheet"/>
+		<link href="assets/css/animate.min.css" rel="stylesheet"/>
+		<link href="assets/css/jquery-ui.css" rel="stylesheet"/>
+        <link href="assets/css/bootstrap.min.css"rel="stylesheet"/>
+        <link href="assets/css/material.min.css" rel="stylesheet"/>
+        <link href="assets/css/angular-material.min.css" rel="stylesheet"/>
+        <link href="assets/css/light-bootstrap-dashboard.css" rel="stylesheet"/>
 
         <!--     Fonts and icons     -->
-        <link href="css/font-awesome.min.css" rel="stylesheet">
-        <link href="css/icon.css" rel="stylesheet">
-        <link href="css/pe-icon-7-stroke.css" rel="stylesheet"/>
-        <link href="css/ng-tags-input.bootstrap.css" rel="stylesheet"/>
-        <link href="css/ng-tags-input.css" rel="stylesheet"/>
-        <link href="css/style.css" rel="stylesheet"/>
-        <link href="css/flaticon.css" type="text/css" rel="stylesheet"/>
-        <link href="css/mdKeyboard.css" rel="stylesheet"/>
+        <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+        <link href="assets/css/icon.css" rel="stylesheet">
+        <link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet"/>
+        <link href="assets/css/ng-tags-input.bootstrap.css" rel="stylesheet"/>
+        <link href="assets/css/ng-tags-input.css" rel="stylesheet"/>
+        <link href="assets/css/style.css" rel="stylesheet"/>
+        <link href="assets/css/flaticon.css" type="text/css" rel="stylesheet"/>
+        <link href="assets/css/mdKeyboard.css" rel="stylesheet"/>
         
         <!--   Core JS Files   -->
-        <script src="js/jquery.3.2.1.min.js"></script>
-        <script src="js/jquery-ui.js"></script>
-		<script src="js/bootstrap.min.js"></script>
-        <script src="js/angular.min.js"></script>
-        <script src="js/angular-aria.min.js"></script>
-        <script src="js/angular-animate.min.js"></script>
-        <script src="js/angular-material.min.js"></script>
-        <script src="js/angular-route.min.js"></script>
-        <script src="js/mdKeyboard.js"></script>
-        <script src="js/ios-dblclick.js"></script>
-        <script src="js/angular-touch.js"></script>
-        <script src='js/jquery.mousewheel.min.js'></script>
+        <script src="assets/js/jquery.3.2.1.min.js"></script>
+        <script src="assets/js/jquery-ui.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/angular.min.js"></script>
+        <script src="assets/js/angular-aria.min.js"></script>
+        <script src="assets/js/angular-animate.min.js"></script>
+        <script src="assets/js/angular-material.min.js"></script>
+        <script src="assets/js/angular-route.min.js"></script>
+        <script src="assets/js/mdKeyboard.js"></script>
+        <script src="assets/js/ios-dblclick.js"></script>
+        <script src="assets/js/angular-touch.js"></script>
+        <script src='assets/js/jquery.mousewheel.min.js'></script>
 
 		<!--  Charts Plugin -->
-		<script src="js/chartist.min.js"></script>
+		<script src="assets/js/chartist.min.js"></script>
 
 		<!--  Notifications Plugin    -->
-		<script src="js/bootstrap-notify.js"></script>
+		<script src="assets/js/bootstrap-notify.js"></script>
 
 		<!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
-		<script src="js/light-bootstrap-dashboard.js?v=1.4.0"></script>
+		<script src="assets/js/light-bootstrap-dashboard.js?v=1.4.0"></script>
 
 		<!-- Stripe Plugin -->
         <script src="https://js.stripe.com/v3/"></script>
@@ -69,7 +69,7 @@
 	
 	<body ng-app="app" ng-controller="mainController">
         <div class="start-screen" ng-if="startApp == false">
-            <img class="image-responsive" src="images/logo.png" style="height: 80px; margin-top: 160px;">
+            <img class="image-responsive" src="assets/images/logo.png" style="height: 80px; margin-top: 160px;">
             <h1 style="color: white">Beplink</h1>
             <h2 style="color: white">Order Management System</h2>
             <input class="form-control" style="width: 240px; margin: 20px auto" type="text"
